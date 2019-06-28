ed13d861a828dd497e6085da52253e15e22d210e
Kelly Pieffer <Kelly.Pieffer@nttdata.com>
2019-06-28 13:39:14 -0500

    first commit

---
+++ SimpleProject/src/StringDemo.java
@@ -0,0 +1,12 @@
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String s1 = "Hello";
+		String s2 = "World";
+		if(s1.equals(s2))
+			System.out.println("String Identical");
+		else 
+			System.out.println("String not identical");
+	}
+}
